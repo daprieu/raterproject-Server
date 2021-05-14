eda2ccb061fa1c275207844344f4fc7eda73c9da
daprieu <daprieu@gmail.com>
2021-05-14 14:22:45 -0500

    server side exposes games and can view all games in postman

--- raterproject/urls.py
+++ raterproject/urls.py
@@ -13,9 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from rest_framework import routers
+
+from raterprojectapi.views import GameView
+from django.conf.urls import include
 from django.urls import path
+from raterprojectapi.views import register_user, login_user
+
+router = routers.DefaultRouter(trailing_slash=False)
+# router.register(r'gametypes', GameTypeView, 'gametype')
+router.register(r'games', GameView, 'game')
+# router.register(r'events', EventView, 'event')
+# router.register(r'profile', Profile, 'profile')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('register', register_user),
+    path('login', login_user),
+    path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
 ]
